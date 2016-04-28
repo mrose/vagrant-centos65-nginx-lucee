44f87018a57b19b366666b3ad0b8fcb8779a1a54
Mitchell Rose <mmrose@cogdata.com>
2016-04-28 13:45:32 -0400

    update zulu

--- provision/zulu/zulu.sh
+++ provision/zulu/zulu.sh
@@ -1,26 +1,19 @@
 #!/bin/sh
 
-runfile="/vagrant/provision/.zulu.provisioned"
-src="http://repos.azulsystems.com/rhel/zulu.repo"
-jdk="zulu-8"
+. /vagrant/provision/provisionable.sh
+provisioned zulu
 
-if [ -f "${runfile}" ]; then
-  echo "zulu provisioning already completed `cat ${runfile}`"
-  echo "exiting zulu provisioning"
-  exit 0
-fi
+SRC="http://repos.azulsystems.com/rhel/zulu.repo"
+JDK="zulu-8"
 
-echo "Provisioning zulu ..."
-
-curl -o /etc/yum.repos.d/zulu.repo ${src}
+curl -o /etc/yum.repos.d/zulu.repo $SRC
 rpm --import /vagrant/provision/zulu/zulu_signing.key
-yum install ${jdk}
+yum install $JDK
 
 # below is so we don't have to reconfigure the tomcat install
-#ln -s ${javahome} /usr/java/default
+#ln -s $JAVA_HOME /usr/java/default
 
-#export JAVA_HOME=${javahome}
+#export JAVA_HOME=$JAVA_HOME
 #java -version
 
-date > "${runfile}"
-echo "Completed java provisioning"
+provisioned zulu
